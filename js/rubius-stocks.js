814abd78726c751479b0b4b50a8ac62ff83e3554
Luke Hoang <lhoang@madebyxds.com>
2021-04-06 23:34:49 -0400

    update

--- js/rubius-stocks.js
+++ js/rubius-stocks.js
@@ -3,14 +3,25 @@ window.addEventListener('DOMContentLoaded', (event) => {
   //   API Calls
   // Get snapshot and prepare data
 //   var apiKey = jsonData[0].key;
-  var apiKey = "S_Z5wqYZOglnJCXkswa3azp5Wj9YRNFF";
-  getSnapshot(apiKey);
-  let dayData = getTrade(apiKey, 'day');
-  let weekData = getTrade(apiKey, 'week');
-  let monthData = getTrade(apiKey, 'month');
-  let yearData = getTrade(apiKey, 'year');
+  let apiKey = "S_Z5wqYZOglnJCXkswa3azp5Wj9YRNFF";
+  let dayData,weekData,monthData,yearData;
 
-  selectedView = "1D";
+	getSnapshot(apiKey);
+	dayData = getTrade(apiKey, 'day');
+	weekData = getTrade(apiKey, 'week');
+	monthData = getTrade(apiKey, 'month');
+	yearData = getTrade(apiKey, 'year');
+
+  setInterval(function(){ 
+	  getSnapshot(apiKey);
+	   dayData = getTrade(apiKey, 'day');
+	   weekData = getTrade(apiKey, 'week');
+	   monthData = getTrade(apiKey, 'month');
+	   yearData = getTrade(apiKey, 'year');
+	   syncToInterval(selectedView);
+  }, 3000);
+
+	selectedView = "1D";
 
   function createSimpleSwitcher(items, activeItem, activeItemChangedCallback) {
   	let switcherElement = document.createElement('div');
@@ -24,7 +35,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
   		itemEl.addEventListener('click', function() {
   			onItemClicked(item);
   			selectedView = item;
-  			setLastBarText(selectedView);
+  			// setLastBarText(selectedView);
   		});
   		switcherElement.appendChild(itemEl);
   		return itemEl;
@@ -81,20 +92,20 @@ window.addEventListener('DOMContentLoaded', (event) => {
       },
     },
   	timeScale: {
-		rightOffset: 50,
+		rightOffset: 12,
 		fixLeftEdge: true,
 		lockVisibleTimeRangeOnResize: true,
 		rightBarStaysOnScroll: true,
-		borderVisible: true,
+		borderVisible: false,
 		borderColor: '#fff000',
 		visible: true,
 		timeVisible: true,
-		secondsVisible: true,
-  		// tickMarkFormatter: (time) => {
-  		// 	const date = new Date(time.year, time.month, time.day);
-  		// 	return date.getFullYear() + '/' + (date.getMonth() + 1) + '/' + date.getDate();
-  		// },
-  	},
+		secondsVisible: false,
+		tickMarkFormatter: (time, tickMarkType, locale) => {
+            const newTime = LightweightCharts.isBusinessDay(time) ? time : moment.unix(time).format('HH:mm');
+            return String(newTime);
+		},
+	  },
   	grid: {
   		horzLines: {
   			color: '#eee',
@@ -116,7 +127,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
         color: 'rgba(32, 38, 46, 0.1)',
         labelVisible: false,
       }
-    },
+	},
   });
   
   document.getElementById('rubius-stock-chart').appendChild(chartElement);
@@ -132,7 +143,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
   	
   	// Compare Last Two Prices to Set Colors
 	let data = await seriesesData.get(interval);  
-	console.log(data);	
+	// console.log(data);	
   	let secLatestPrice = data[data.length - 2].value;
   	let latestPrice = data[data.length - 1].value;
   	
@@ -149,7 +160,40 @@ window.addEventListener('DOMContentLoaded', (event) => {
     	bottomColor = 'rgba(228, 194, 196, 0.0)';
     	lineColor = 'rgba(196, 33, 45, 1.0)';
   	}
-  	
+
+	chart.timeScale().fitContent();
+	if(interval == '1D' || interval == '1W'){
+		chart.applyOptions({
+			timeScale: {
+				rightOffset: 12,
+				tickMarkFormatter: (time, tickMarkType, locale) => {
+					const newTime = LightweightCharts.isBusinessDay(time) ? time : moment.unix(time).format('HH:mm');
+					return String(newTime);
+				},
+			},
+		});
+	}else if(interval == '1M'){
+		chart.applyOptions({
+			timeScale: {
+				rightOffset: 1,
+				tickMarkFormatter: (time) => {
+					const date = new Date(time.year, time.month, time.day);
+					return date.getMonth() + '/' + date.getDate();
+				},
+			},
+		});
+	}else if(interval == '1Y'){
+		chart.applyOptions({
+			timeScale: {
+				rightOffset: 1,
+				tickMarkFormatter: (time) => {
+					const date = new Date(time.year, time.month, time.day);
+					return date.getMonth() + '/' + date.getDate() + '/' + date.getFullYear();
+				},
+			},
+		});
+	}
+	  
   	areaSeries = chart.addAreaSeries({
       topColor: topColor,	
       bottomColor: bottomColor,
@@ -178,7 +222,13 @@ window.addEventListener('DOMContentLoaded', (event) => {
   		if (param.point === undefined || !param.time || param.point.x < 0 || param.point.x > chartElement.clientWidth || param.point.y < 0 || param.point.y > chartElement.clientHeight) {
   			toolTip.style.display = 'none';
   		} else {
-  			const dateStr = businessDayToString(param.time);
+			var dateTest = moment(param.time);
+			if(dateTest._i['day']){
+				// console.log(dateTest._i);
+				var dateStr = param.time.month+'/'+param.time.day+'/'+param.time.year;
+			}else{
+				var dateStr = moment.unix(param.time).format('MM/DD/YY HH:mm');
+			}
   			toolTip.style.display = 'block';
   			let price = param.seriesPrices.get(areaSeries);
   			toolTip.innerHTML = '<div style="color: #c4212d">Rubius Therapeutics Inc.</div><div style="font-size: 24px; margin: 4px 0px; color: #21384d">' + Math.round(100 * price) / 100 + '</div><div style="color: #21384d">' + dateStr + '</div>';
@@ -241,47 +291,72 @@ window.addEventListener('DOMContentLoaded', (event) => {
 
   //Get SnapShot of a ticker (RUBY)
 	function getSnapshot (apiKey) {
+
+		let currentPrice = 0;
+		let todayOpen = 0;
+		let todaysChange = 0;
+		let todaysChangePercent = 0;
+
+		//CHECK MARKET STATUS
+		//GET CURRENT PRICE/ASK-PRICE
 		fetch('https://api.polygon.io/v2/snapshot/locale/us/markets/stocks/tickers/RUBY?&apiKey='+apiKey)
 		.then(
 			response => response.json()
 		).then(
 			data => {
 				let tickerSnapshot = data.ticker;
+				let tickerLastQuote = tickerSnapshot.lastQuote;
+				let tickerDay = tickerSnapshot.day;
+				let tickerPrevDay = tickerSnapshot.prevDay;
+				let tickerTodayChange = tickerSnapshot.todaysChange;
+				let tickerTodayChangePercent = tickerSnapshot.todaysChangePerc;
 				// console.log(tickerSnapshot);
-
 				// manipulate the DOM
-				document.querySelector('.rubius-stock-price_ticker').innerHTML = tickerSnapshot.ticker;
+				document.querySelector('.rubius-stock-price_price').innerHTML = "$"+tickerLastQuote["P"];
+				document.querySelector('.rubius-stock-details_col-value_open').innerHTML = tickerDay.o?tickerDay.o:'N/A';
+				document.querySelector('.rubius-stock-details_col-value_high').innerHTML = tickerDay.h?tickerDay.h:'N/A';
+				document.querySelector('.rubius-stock-details_col-value_low').innerHTML = tickerDay.l?tickerDay.l:'N/A';
+				document.querySelector('.rubius-stock-details_col-value_volume').innerHTML = tickerDay.v?tickerDay.v.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","):'N/A';
 
-				document.querySelector('.rubius-stock-price_price').innerHTML = "$"+tickerSnapshot.lastQuote['P'];
+				document.querySelector('.rubius-stock-details_col-value_prev-close').innerHTML = tickerPrevDay.c?tickerPrevDay.c:'N/A';
 
-				document.querySelector('.rubius-stock-price_change').innerHTML = tickerSnapshot.todaysChange;
-
-				
-				if(tickerSnapshot.todaysChange >= 0){
+				if(tickerTodayChange > 0){
 					document.querySelector('.rubius-stock-price_change').classList.add('green');
-					document.querySelector('.rubius-stock-price_change').innerHTML = "+" + tickerSnapshot.todaysChange + " " + "(+" + tickerSnapshot.todaysChangePerc  + "%)";
+					document.querySelector('.rubius-stock-price_change').innerHTML = "+" + tickerTodayChange + " " + "(+" + tickerTodayChangePercent  + "%)";
 				}else{
 					document.querySelector('.rubius-stock-price_change').classList.add('red');
-					document.querySelector('.rubius-stock-price_change').innerHTML = "-" + tickerSnapshot.todaysChange + " " + "(-" + tickerSnapshot.todaysChangePerc  + "%)";
+					document.querySelector('.rubius-stock-price_change').innerHTML = tickerTodayChange + " " + "(" + tickerTodayChangePercent  + "%)";
 				}
-
-				document.querySelector('.rubius-stock-details_col-value_open').innerHTML = tickerSnapshot.day['o'];
-				document.querySelector('.rubius-stock-details_col-value_prev-open').innerHTML = tickerSnapshot.prevDay['o'];
-
-				document.querySelector('.rubius-stock-details_col-value_high').innerHTML = tickerSnapshot.day['h'];
-				document.querySelector('.rubius-stock-details_col-value_prev-high').innerHTML = tickerSnapshot.prevDay['h'];
-				
-				document.querySelector('.rubius-stock-details_col-value_low').innerHTML = tickerSnapshot.day['l'];
-				document.querySelector('.rubius-stock-details_col-value_prev-low').innerHTML = tickerSnapshot.prevDay['l'];
-				
-
-				document.querySelector('.rubius-stock-details_col-value_close').innerHTML = tickerSnapshot.day['c'];
-				document.querySelector('.rubius-stock-details_col-value_prev-close').innerHTML = tickerSnapshot.prevDay['c'];
-
-				document.querySelector('.rubius-stock-details_col-value_volume').innerHTML = tickerSnapshot.day['v'].toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-				document.querySelector('.rubius-stock-details_col-value_prev-volume').innerHTML = tickerSnapshot.prevDay['v'].toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 			}
 		);
+
+
+		
+
+
+		//GET PRE DAY SNAPSHOT
+		// preDay = moment().subtract(1, 'days').format("YYYY-MM-DD");
+		// fetch('https://api.polygon.io/v2/aggs/ticker/RUBY/prev?unadjusted=true&apiKey='+apiKey)
+		// .then(
+		// 	response => response.json()
+		// ).then(
+		// 	data => {
+		// 		let tickerSnapshot = data.results[0];
+		// 		console.log(tickerSnapshot);
+		// 		// manipulate the DOM
+		// 		// document.querySelector('.rubius-stock-details_col-value_prev-open').innerHTML = tickerSnapshot.o?tickerSnapshot.o:'N/A';
+		// 		// document.querySelector('.rubius-stock-details_col-value_prev-high').innerHTML = tickerSnapshot.h?tickerSnapshot.h:'N/A';
+		// 		// document.querySelector('.rubius-stock-details_col-value_prev-low').innerHTML = tickerSnapshot.l?tickerSnapshot.l:'N/A';
+				
+		// 		// document.querySelector('.rubius-stock-details_col-value_prev-volume').innerHTML = tickerSnapshot.v?tickerSnapshot.v.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","):'N/A';
+		// 	}
+		// );
+
+		
+		
+
+				
+				
 	}
 
 
@@ -292,19 +367,19 @@ window.addEventListener('DOMContentLoaded', (event) => {
 		if(chartType == 'day'){
 			multiplier = 5;
 			timeSpan = 'minute';
-			day_from = moment().subtract(5, 'days').format("YYYY-MM-DD");
+			day_from = moment().subtract(0, 'days').format("YYYY-MM-DD");
 		}else if(chartType == 'week'){
 			multiplier = 30;
 			timeSpan = 'minute';
-			day_from = moment().subtract(4, 'weeks').format("YYYY-MM-DD");
+			day_from = moment().subtract(5, 'days').format("YYYY-MM-DD");
 		}else if(chartType == 'month'){
 			multiplier = 1;
 			timeSpan = 'day';
-			day_from = moment().subtract(6, 'months').format("YYYY-MM-DD");
+			day_from = moment().subtract(30, 'days').format("YYYY-MM-DD");
 		}else if(chartType == 'year'){
 			multiplier = 1;
 			timeSpan = 'day';
-			day_from = moment().subtract(2, 'years').format("YYYY-MM-DD");
+			day_from = moment().subtract(360, 'days').format("YYYY-MM-DD");
 		}
 		day_to = moment().format("YYYY-MM-DD");
 		return fetch('https://api.polygon.io/v2/aggs/ticker/RUBY/range/'+multiplier+'/'+timeSpan+'/'+day_from+'/'+day_to+'?unadjusted=true&sort=asc&limit=50000&apiKey='+apiKey)
@@ -314,10 +389,19 @@ window.addEventListener('DOMContentLoaded', (event) => {
 			data => {
 				let results = data.results;
 				results.forEach(result => {
-					dayDataResult.push(
-						{ time: result.t/1000, value: result.c }
-					);
+					// console.log(moment.unix(result.t/1000).format('YYYY-MM-DD HH:mm'));
+					if(chartType == 'month' || chartType == 'year'){
+						dayDataResult.push(
+							{ time: moment.unix(result.t/1000).format('YYYY-MM-DD'), value: result.c }
+						);
+					}else{
+						dayDataResult.push(
+							{ time: result.t/1000, value: result.c }
+						);
+					}
 				});
+				// console.log(dayDataResult);
+
 				return dayDataResult;
 			}
 		)
